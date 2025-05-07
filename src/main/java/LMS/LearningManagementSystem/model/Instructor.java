dcf282e662fc9c9b387bd7f65e71bf7d249b0bf5
AhmedEmad6004 <ah5900658@gmail.com>
2025-05-07 23:03:58 +0200

    Update Instructor.java
    package LMS.LearningManagementSystem.model;
    
    import jakarta.persistence.*;
    import lombok.AllArgsConstructor;
    import lombok.NoArgsConstructor;
    import lombok.experimental.SuperBuilder;
    import org.springframework.security.core.GrantedAuthority;
    import org.springframework.security.core.authority.SimpleGrantedAuthority;
    
    import java.io.Serializable;
    import java.util.Collection;
    import java.util.List;
    
    @Entity
    @Table(name = "instructors")
    @AllArgsConstructor
    @NoArgsConstructor
    @SuperBuilder
    public class Instructor extends User implements Serializable {
    
        private static final long serialVersionUID = 1L;
    
        @OneToMany(mappedBy = "instructor")
        private transient List<Course> createdCourses;
    
        @Enumerated(EnumType.STRING)
        protected Role role;
    
        public Instructor(String name, String email, String password, Role role) {
            super(name, email, password);
            this.role = Role.Instructor;
        }
    
        @Override
        public Collection<? extends GrantedAuthority> getAuthorities() {
            return List.of(new SimpleGrantedAuthority(role.name()));
        }
    }

--- src/main/java/LMS/LearningManagementSystem/model/Instructor.java
+++ src/main/java/LMS/LearningManagementSystem/model/Instructor.java
@@ -7,26 +7,29 @@ import lombok.experimental.SuperBuilder;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
-import java.net.ProtocolFamily;
-import java.util.List;
+import java.io.Serializable;
 import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "instructors")
 @AllArgsConstructor
 @NoArgsConstructor
 @SuperBuilder
-public class Instructor extends User{
+public class Instructor extends User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
     @OneToMany(mappedBy = "instructor")
-    private List<Course> createdCourses;
+    private transient List<Course> createdCourses;
+
     @Enumerated(EnumType.STRING)
     protected Role role;
 
-    public Instructor(String name, String email, String password,Role role) {
+    public Instructor(String name, String email, String password, Role role) {
         super(name, email, password);
         this.role = Role.Instructor;
     }
-
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
